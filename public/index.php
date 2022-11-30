f66e63799c54158a6f63646b47b89b35b6d34817
BariqPribadi <bariqunggul@gmail.com>
2022-11-30 20:11:08 +0700

    Assignment Pameo

---
+++ public/index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>CV Bariq Unggul Pribadi</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Tinos:wght@400;700&display=swap" rel="stylesheet">
+    </head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <body>
+        <div class="container page">
+            <div class="row" >
+                <div style="border-bottom: 3px solid black; padding: 0;">
+                    <p class="p1">BARIQ UNGGUL PRIBADI</p>
+                    <p class="p2" style="margin-top: -20px;">Tembelang, Rojoimo, Wonosobo, Central Java, 56315 
+                        | +628999389398 | bariqunggul@gmail.com | https://github.com/BariqPribadi</p>
+                </div>
+                <div style="border-bottom: 3px solid black; padding: 0;">
+                    <p class="p1" style="margin-top: 20px;">Profile</p>
+                    <p class="p2" style="margin-top: -10px;">I am Bariq Unggul Pribadi, final year college student Diponegoro University majoring in Computer Engineering. My last activity was being an intern at startup kepul.id, working on the front-end website. I have an interest working in the field of web development, especially front-end development.</p>
+                </div>
+                <div style="border-bottom: 3px solid black; padding: 0;">
+                    <p class="p1">Education</p>
+                    <p class="p2" style="margin-top: -10px; font-weight: 700;">Diponegoro University (2018 - present)</p>
+                    <p class="p2" style="margin-top: -10px;">Bachelor of Compter Engineering – Cumulative GPA 3.93</p>
+                    <div class="d-flex justify-content-end">
+                        <div class="col-1 div1" >
+                            <p class="dot"></p>
+                        </div>
+                        <div class="col-11">
+                            <p class="p2" style="margin-top: -10px;">Understand the PHP programming language, have basic knowledge of the Laravel Framework and CodeIgniter4, and understand the use of the MySQL database server.</p>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <div class="col-1 div1" >
+                            <p class="dot"></p>
+                        </div>
+                        <div class="col-11">
+                            <p class="p2" style="margin-top: -10px;">Understand the use of HTML, CSS and Bootstrap.</p>
+                        </div>
+                    </div>
+                    <p class="p2" style="margin-top: -10px; font-weight: 700;">2 Senior High School Wonosobo (2015 - 2018)</p>
+                    <p class="p2" style="margin-top: -10px;">Specialization subjects of natural science.</p>
+                </div>
+                <div style="border-bottom: 3px solid black; padding: 0;">
+                    <p class="p1">Experience</p>
+                    <p class="p2" style="margin-top: -10px; font-weight: 700;">Intern at Startup kepul.id (August 2021 - October 2021)</p>
+                    <div class="d-flex justify-content-end">
+                        <div class="col-1 div1" >
+                            <p class="dot"></p>
+                        </div>
+                        <div class="col-11">
+                            <p class="p2" style="margin-top: -10px;">Working on the front-end of the kepul.id website using the CodeIgniter4 Framework.</p>
+                        </div>
+                    </div>
+                </div>
+                <div style="border-bottom: 3px solid black; padding: 0;">
+                    <p class="p1 ">Skills</p>
+                    <div class="d-flex justify-content-end">
+                        <div class="col-1 div1" >
+                            <p class="dot"></p>
+                        </div>
+                        <div class="col-11">
+                            <p class="p2" style="margin-top: -10px;">Working on the front-end of the website.</p>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <div class="col-1 div1" >
+                            <p class="dot"></p>
+                        </div>
+                        <div class="col-11">
+                            <p class="p2" style="margin-top: -10px;">Basic English.</p>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <div class="col-1 div1" >
+                            <p class="dot"></p>
+                        </div>
+                        <div class="col-11">
+                            <p class="p2" style="margin-top: -10px;">Optimizing, maintaining, troubleshooting, and improving websites.</p>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <div class="col-1 div1" >
+                            <p class="dot"></p>
+                        </div>
+                        <div class="col-11">
+                            <p class="p2" style="margin-top: -10px;">Able to work in a team and have good communication skills.</p>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <div class="col-1 div1" >
+                            <p class="dot"></p>
+                        </div>
+                        <div class="col-11">
+                            <p class="p2" style="margin-top: -10px;">Can work under pressure.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+
+    <style>
+        .p1{
+            font-family: 'Tinos';
+            font-weight: 700;
+            font-size: 22px;
+            color: black;
+            margin-top: 0;
+            text-align: justify;
+        }
+        .p2{
+            font-family: 'Tinos';
+            font-weight: 400;
+            font-size: 16px;
+            color: black;
+            display: block;
+            text-align: justify;
+        }
+        body {
+        margin: 0;
+        padding: 0;
+        background-color: #FAFAFA;
+        }
+
+        * {
+        box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        }
+
+        .page {
+        width: 21cm;
+        height: 29.7cm;
+        padding: 3cm 3cm 3cm 4cm;
+        margin: 1cm auto;
+        border: 1px #D3D3D3 solid;
+        background: white;
+        box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+        line-height: 1.5;
+        overflow: auto;
+        }
+
+        @page {
+        size: A4;
+        margin: 0;
+        }
+        .dot {
+        height: 6px;
+        width: 6px;
+        background-color: black;
+        border-radius: 50%;
+        overflow: auto;
+        margin-left: -10px;
+        }
+        .div1{
+            height: 10px;
+            width: 10px;
+        }
+    </style>
+</html>
